cf0412896dcdb871e483565b3a334afaf7d6aab7
DanmoSAMA <772523546@qq.com>
2022-03-26 22:56:01 +0800

    feat(添加svg-sprite)

--- werewolf-fe/src/App.tsx
+++ werewolf-fe/src/App.tsx
@@ -1,9 +1,11 @@
 import './App.scss'
+import SvgIcon from './components/SvgIcon/index'
 
 function App() {
   return (
     <div>
       hello world
+      <SvgIcon name="hunter"/>
     </div>
   )
 }
--- werewolf-fe/vite.config.ts
+++ werewolf-fe/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { svgBuilder } from './src/plugins/svgBuilder';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), svgBuilder('./public/assets/icons/')]
 })
