5f8498b5ea9e3486328b13116e4034b96e110bea
DanmoSAMA <772523546@qq.com>
2022-04-16 17:01:10 +0800

    feat(完成Home)

--- werewolf-fe/src/App.tsx
+++ werewolf-fe/src/App.tsx
@@ -1,14 +1,10 @@
 import { Route, Routes, Navigate } from 'react-router-dom'
-import loadable from '@loadable/component'
 import routes from '@/routes'
 import './App.scss'
-
-import Home from '@/pages/Home/index'
 
 function App() {
   return (
     <div>
-      hello
       <Routes>
         <Route path="/" element={<Navigate to="/home" replace />} />
         {routes.map((item, i) => {
--- werewolf-fe/src/pages/Home/index.tsx
+++ werewolf-fe/src/pages/Home/index.tsx
@@ -1,11 +1,28 @@
 import SvgIcon from '@/components/SvgIcon/index'
+import { useNavigate } from "react-router-dom";
 import './index.scss'
 
 function Home() {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <SvgIcon name="hunter" />
-      hello world
+    <div className={'home'}>
+      <div className={'home-header'}>
+        <div className={'home-header-icon'}>
+          <SvgIcon name="werewolf" />
+        </div>
+        <div className={'home-header-title'}>
+          狼人杀
+        </div>
+      </div>
+      <div className={'home-body'}>
+        <div className={'home-body-join_room'} onClick={() => navigate('/join')}>
+          加入房间
+        </div>
+        <div className={'home-body-create_room'} onClick={() => navigate('/create')}>
+          创建房间
+        </div>
+      </div>
     </div>
   )
 }
